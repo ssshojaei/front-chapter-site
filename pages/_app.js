78d483ebe3db7a703564a61079f5d55397c460c7
Saleh Shojaei <ordinarysaleh@pm.me>
2021-05-15 19:34:20 +0430

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+import "../scss/main.scss";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
