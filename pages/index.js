bc7ac96f864a1f02c0f484a9cf17615e9a89128a
Saleh Shojaei <ordinarysaleh@pm.me>
2021-12-27 13:29:10 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
 import React from "react";
+import Head from "next/head";
 
 const Home = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100">
-      <div className="d-flex flex-column align-items-center">
-        <h2 className="text-primary">فرانت چپتر</h2>
-        <h4 className="text-secondary">به زودی سایتمون راه می‌افته</h4>
-        <h5 className="text-info">
-          <a href="https://t.me/frontChapter">T.me/FrontChapter</a>
-          <a href="https://instagram.com/frontChapter">instagram.com/FrontChapter</a>
-        </h5>
+    <>
+        <Head>
+          <title>FrontChapter | فرانت چپتر</title>
+        </Head>
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <div className="d-flex flex-column align-items-center">
+          <h2 className="text-primary">فرانت چپتر</h2>
+          <h4 className="text-secondary">به زودی سایتمون راه می‌افته</h4>
+          <h5 className="text-info">
+            <a href="https://t.me/frontChapter">T.me/FrontChapter</a>
+            <br />
+            <a href="https://instagram.com/frontChapter">instagram.com/FrontChapter</a>
+          </h5>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
