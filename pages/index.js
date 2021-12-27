9b2037c0b1ed298e5ddb0938e5eada0331159159
Saleh Shojaei <ordinarysaleh@pm.me>
2021-12-27 13:30:44 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,14 @@ const Home = () => {
         <div className="d-flex flex-column align-items-center">
           <h2 className="text-primary">فرانت چپتر</h2>
           <h4 className="text-secondary">به زودی سایتمون راه می‌افته</h4>
-          <h5 className="text-info">
-            <a href="https://t.me/frontChapter">T.me/FrontChapter</a>
-            <br />
-            <a href="https://instagram.com/frontChapter">instagram.com/FrontChapter</a>
-          </h5>
+          <div className="text-center">
+            <h5 className="text-info">
+              <a href="https://t.me/frontChapter">T.me/FrontChapter</a>
+            </h5>
+            <h5 className="text-danger">
+              <a href="https://instagram.com/frontChapter">instagram.com/FrontChapter</a>
+            </h5>
+          </div>
         </div>
       </div>
     </>
